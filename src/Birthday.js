7a2e9a56416a4c43ef58df9da1551ebabdb29e95
Your Name <you@example.com>
2020-10-23 22:27:33 +0530

    commit 2 - useEffect, dynamic values

--- src/Birthday.js
+++ src/Birthday.js
@@ -1,42 +1,100 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import birthdays from './birthdays'
 
 const Birthday = () => {
-    const [person, setPerson] = React.useState(birthdays);
-    const filterToday = () => {
-        let d = new Date()
-        let dayToday = d.getDate()
-
-        let todays = person.filter((people) => people.bday === dayToday & people.bmonth === 'October')
-        setPerson(todays)
-    }
- 
-    return <React.Fragment>
-         <section className="container">
-
-<h1 style={{color:'teal'}}>Today's Birthdays</h1>
-        { person.map(eachPerson => {const{name, bday, bmonth, pic} = eachPerson
-           
-        return(   
-            <article key={eachPerson.id} className="people">
-                <img alt={name} src={pic}></img>
-                <div className="dets">
-                    <h4>{name}</h4>
-                    <p>{bday} {bmonth}</p>
-                </div>
-            </article>
-
-
-        )
-        })
-        
+    const [people, setPeople] = useState(birthdays)
+    const [thisMonth, setThisMonth] = useState('');
+    let d = new Date()
+    let today = d.getDate()
+    let theMonth = d.getMonth()
+    
+    const findMonth = () => {
+        if(theMonth===0){
+            setThisMonth('January')
         }
-        <button onClick={()=>{alert('Wishes sent!')}}>Wish All</button>
-        <button onClick={filterToday}>Filter Today All</button>
+        else if(theMonth===1){
+            setThisMonth('February')
+        } 
+        else if(theMonth===2){
+            setThisMonth('March')
+        } 
+        else if(theMonth===3){
+            setThisMonth('April')
+        } 
+        else if(theMonth===4){
+            setThisMonth('May')
+        } 
+        else if(theMonth===5){
+            setThisMonth('June')
+        } 
+        else if(theMonth===6){
+            setThisMonth('July')
+        } 
+        else if(theMonth===7){
+            setThisMonth('August')
+        } 
+        else if(theMonth===8){
+            setThisMonth('September')
+        } 
+        else if(theMonth===9){
+            setThisMonth('October')
+        } 
+        else if(theMonth===10){
+            setThisMonth('November')
+        } 
+        else{
+            setThisMonth('December')
+        }
+    }
+
+    const filterToday = () => {
+        
+    let todays = people.filter((persons) => persons.bday === today && persons.bmonth === thisMonth)
+    setPeople(todays)
+     
+  }
+
+  const filterThisMonth = () => {
+      let months = people.filter((persons) => persons.bmonth===thisMonth)
+      setPeople(months)
+  }
+
+    useEffect(() => {
+        findMonth()
+    });
 
 
-        </section>
-    </React.Fragment>
+    return <>
+    <section style={{background:'white', width: '50%', margin:'30px auto',padding:'30px', borderRadius:'10px'}}>
+    <h1 style={{color:'teal'}}>Birthdays!    <button onClick={filterToday}>Today</button>
+    <button onClick={filterThisMonth}>This Month</button>
+</h1>
+
+  
+
+        {people.map(person => {const{id, name, bday, bmonth,pic} = person
+            return(
+                <article style={{paddingBottom:'20px'}}  key={id}>
+                    <img  alt={`${id}-${name}`} src={pic}/>
+                    {bday===today && bmonth===thisMonth && <button onClick={()=>{alert(`Wishes sent to ${name}!`)}}> Wish </button>}
+                    <div>
+            {bday===today && bmonth===thisMonth ? <h2><i style={{color:'gold'}}>Today is {name}'s birthday!</i></h2> : <p>{name}, {bday} {bmonth}</p>}
+                       
+                    </div>
+                    
+                </article>
+            )
+        
+        })}
+    <button onClick={()=>setPeople(birthdays)}>View all</button>
+
+    </section>
+        
+
+        </>
+
+  
 }
+
 
 export default Birthday
